33f6586bea72362442cb2f21639efe2883bdf96d
David Potgieter <david@weswingcats.com>
2023-06-11 23:38:13 +0700

    config changes for ga

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-import App from './src/App';
+import { AppRegistry } from 'react-native'
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json'
+import App from './src/App'
+
+AppRegistry.registerComponent(appName, () => App)
