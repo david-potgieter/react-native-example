029e9c49f2e98ca28b32d379d51f92e55a7b9490
David Potgieter <david@weswingcats.com>
2023-06-12 14:03:45 +0700

    coverage comments not working

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
   preset: 'react-native',
+  collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
