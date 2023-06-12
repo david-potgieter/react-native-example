5e8c335c829b0d4c5f999185d8bab8ba5d9ad3a2
David Potgieter <david@weswingcats.com>
2023-06-12 18:46:30 +0700

    added podium test

--- __tests__/BaseTextInput.test.tsx
+++ __tests__/BaseTextInput.test.tsx
@@ -1,36 +1,33 @@
-import { fireEvent, render } from '@testing-library/react-native'
+import { fireEvent, render, waitFor } from '@testing-library/react-native'
 import { useResetAtom } from 'jotai/utils'
-import { NativeBaseProvider } from 'native-base'
 import React from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { BaseTextInput } from '../src/components/form/input/BaseTextInput'
+import { TestProvidersWrapper } from '../src/utils/providers/test/TestProviders'
 
 jest.mock('@fortawesome/react-native-fontawesome', () => ({
   FontAwesomeIcon: '',
 }))
 
-const inset = {
-  frame: { x: 0, y: 0, width: 0, height: 0 },
-  insets: { top: 0, left: 0, right: 0, bottom: 0 },
-}
-
 describe('BaseTextInput', () => {
   const TestWrapper = () => {
     const form = useForm({ defaultValues: { searchString: '' } })
     return (
-      <NativeBaseProvider initialWindowMetrics={inset}>
+      <TestProvidersWrapper>
         <FormProvider {...form}>
           <BaseTextInput control={form.control} name="searchString" />
         </FormProvider>
-      </NativeBaseProvider>
+      </TestProvidersWrapper>
     )
   }
 
-  test('renders correctly and clears search string', () => {
+  test('renders correctly and clears search string', async () => {
     const { getByPlaceholderText, queryByTestId } = render(<TestWrapper />)
     const input = getByPlaceholderText('Search for a user name...')
     fireEvent.changeText(input, 'John Doe')
-    expect(input.props.value).toBe('John Doe')
+    await waitFor(() => {
+      expect(input.props.value).toBe('John Doe')
+    })
     const clearButton = queryByTestId('clearButton')
     if (clearButton) {
       fireEvent.press(clearButton)
---
+++ __tests__/PlayerPodium.test.tsx
@@ -0,0 +1,45 @@
+import { render, waitFor } from '@testing-library/react-native'
+import React from 'react'
+import { PlayerPodium } from '../src/components/ui/PlayerPodium'
+import { PlayerPodiumProps } from '../src/types/app-types'
+import { TestProvidersWrapper } from '../src/utils/providers/test/TestProviders'
+
+describe('PlayerPodium', () => {
+  const TestWrapper = (props: PlayerPodiumProps) => {
+    return (
+      <TestProvidersWrapper>
+        <PlayerPodium rank={props.rank} isSearchedUser={props?.isSearchedUser} />
+      </TestProvidersWrapper>
+    )
+  }
+  it('renders rank correctly: 1st', async () => {
+    const { getByTestId, getByText } = render(<TestWrapper rank={1} />)
+    await waitFor(() => {
+      expect(getByText('1')).toBeTruthy()
+      expect(getByTestId('podiumPlace')?.props?.style.backgroundColor).toBe('#fde047')
+    })
+  })
+
+  it('renders rank correctly: 2nd', async () => {
+    const { getByTestId, getByText } = render(<TestWrapper rank={2} />)
+    await waitFor(() => {
+      expect(getByText('2')).toBeTruthy()
+      expect(getByTestId('podiumPlace')?.props?.style.backgroundColor).toBe('#e7e5e4')
+    })
+  })
+  it('renders rank correctly: 6th', async () => {
+    const { getByTestId, getByText } = render(<TestWrapper rank={6} isSearchedUser />)
+    await waitFor(() => {
+      expect(getByText('6')).toBeTruthy()
+      expect(getByTestId('podiumPlace')?.props?.style.backgroundColor).toBe('#bae6fd')
+    })
+  })
+
+  it('renders rank correctly: 8th', async () => {
+    const { getByTestId, getByText } = render(<TestWrapper rank={8} />)
+    await waitFor(() => {
+      expect(getByText('8')).toBeTruthy()
+      expect(getByTestId('podiumPlace')?.props?.style.backgroundColor).toBe('#f5f5f5')
+    })
+  })
+})
